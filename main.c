11531f74c8065d5a2e1f26c32f9e5432c00dd71f
Snerksss <berndts.tin21@student.dhbw-heidenheim.de>
2022-02-01 11:06:15 +0100

    FunktionenNamen Großgeschrieben

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ int numberLR;
 int sqrLR;
 
 
-void showGameField(int field[9][9]) {
+void ShowGameField(int field[9][9]) {
     printf("     A B C   D E F   G H I \n");
     printf("     -----   -----   ----- \n");
     for (int i = 0; i < 9; i++) {
@@ -56,7 +56,7 @@ void showGameField(int field[9][9]) {
     }
 }
 
-int indexOf(char alphabet[9], char buchstabe) {
+int IndexOf(char alphabet[9], char buchstabe) {
     for (int i = 0; i < 9; i++) {
         if (alphabet[i] == buchstabe) {
             return i;
@@ -65,7 +65,7 @@ int indexOf(char alphabet[9], char buchstabe) {
     return 10;
 }
 
-int checkLine(int line, int row, int zahl) {
+int CheckLine(int line, int row, int zahl) {
     for (int i = 0; i < 9; i++) {
         if (gameArea[line][i] == zahl) {
             return 0;
@@ -74,7 +74,7 @@ int checkLine(int line, int row, int zahl) {
     return 1;
 }
 
-int checkRow(int line, int row, int zahl) {
+int CheckRow(int line, int row, int zahl) {
     for (int i = 0; i < 9; i++) {
         if (gameArea[i][row] == zahl) {
             return 0;
@@ -83,7 +83,7 @@ int checkRow(int line, int row, int zahl) {
     return 1;
 }
 
-int checkBox(int line, int row, int zahl) {
+int CheckBox(int line, int row, int zahl) {
     int tmpLine = line / 3;
     int tmpRow = row / 3;
     tmpRow *= 3;
@@ -98,14 +98,14 @@ int checkBox(int line, int row, int zahl) {
     return 1;
 }
 
-int checkRules(int line, int row, int zahl) {
-    if (checkRow(line, row, zahl) == 1 && checkLine(line, row, zahl) == 1 && checkBox(line, row, zahl) == 1) {
+int CheckRules(int line, int row, int zahl) {
+    if (CheckRow(line, row, zahl) == 1 && CheckLine(line, row, zahl) == 1 && CheckBox(line, row, zahl) == 1) {
         return 1;
     }
     return 0;
 }
 
-int checkGamePlay() {
+int CheckGamePlay() {
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
             if (gameArea[i][j] != kontrollArea[i][j]) {
@@ -116,11 +116,11 @@ int checkGamePlay() {
     return 1;
 }
 
-void play() {
-    showGameField(gameArea);
+void PlayerInput() {
+    ShowGameField(gameArea);
     int bool = 1;
     char alphabet[9] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'};
-    while (checkGamePlay() == 0) {
+    while (CheckGamePlay() == 0) {
         int y = 0;
         int x = 0;
         char buchstabe;
@@ -134,27 +134,27 @@ void play() {
         for (int i = 0; i < 25; i++) {
             printf("\n");
         }
-        x = indexOf(alphabet, buchstabe);
+        x = IndexOf(alphabet, buchstabe);
         if (zahl > 0 && zahl < 10 && x >= 0 && x != 10)
             if (gameField[y - 1][x] == 0)
                 gameArea[y - 1][x] = zahl;
 
-        showGameField(gameField);
+        ShowGameField(gameField);
         printf("\n");
         printf("\n");
         printf("\n");
-        showGameField(gameArea);
+        ShowGameField(gameArea);
     }
 }
 
 //Befuellt ein 3x3 Feld abhaengig von der gegebenen StartLine und StartRow
-void squareMatrix(int startLine, int startRow) {
+void SquareMatrix(int startLine, int startRow) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             int num;
             do {
                 num = (rand() % 9) + 1;
-            } while (checkBox(startLine, startRow, num) == 0);
+            } while (CheckBox(startLine, startRow, num) == 0);
             gameArea[startLine + i][startRow + j] = num;
         }
     }
@@ -163,7 +163,7 @@ void squareMatrix(int startLine, int startRow) {
 
 //Befuellt Rekursiv die fehlenden Felder
 //braucht keinen Zufall, da dieser bereits durch die gegebenen 3x3 Felder gegeben ist, denn hier haengen alles Zahlen von den 3x3 Feldern ab
-int rekursivFill(int line, int row) {
+int RekursivFill(int line, int row) {
 
     // Abbruchkriterium, alles ist dann befuellt
     if (row >= 9 && line >= 9)
@@ -197,9 +197,9 @@ int rekursivFill(int line, int row) {
 
 
     for (int testZahl = 1; testZahl <= numberLR; testZahl++) {
-        if (checkRules(line, row, testZahl) == 1) {
+        if (CheckRules(line, row, testZahl) == 1) {
             gameArea[line][row] = testZahl;
-            if (rekursivFill(line, row + 1) == 1) {
+            if (RekursivFill(line, row + 1) == 1) {
                 //Uebergabe des Abbruchs bis zum ersten Aufruf
                 return 1;
             }
@@ -207,14 +207,14 @@ int rekursivFill(int line, int row) {
             gameArea[line][row] = 0;
 
             //Test um zu sehen wie oft es nicht geklappt hat nicht relevant fuer die Berechnung
-            //showGameField();
+            //ShowGameField();
 
         }
     }
     return 0;
 }
 
-void clearField(int numberOfClear) {
+void ClearField(int numberOfClear) {
     for (int i = 0; i < numberOfClear; i++) {
         int line = rand() % 10;
         int row = rand() % 10;
@@ -226,7 +226,7 @@ void clearField(int numberOfClear) {
     }
 }
 
-void copyField() {
+void CopyField() {
     for (int i = 0; i < numberLR; i++) {
         for (int j = 0; j < numberLR; j++) {
             kontrollArea[i][j] = gameArea[i][j];
@@ -234,7 +234,7 @@ void copyField() {
     }
 }
 
-void copyPlayerField() {
+void CopyPlayerField() {
     for (int i = 0; i < numberLR; i++) {
         for (int j = 0; j < numberLR; j++) {
             gameField[i][j] = gameArea[i][j];
@@ -242,26 +242,26 @@ void copyPlayerField() {
     }
 }
 
-void genNew() {
+void GenNew() {
     srand(time(NULL));
     for (int i = 0; i < numberLR; i = i + sqrLR) {
         //es werden die 3x3 Felder oben links, mitte und unten rechts befuellt
         //Die 3x3 Felder sind die einzigen die in Line und Row von nichts abhaengen
         //somit ist der Zufall des Sodokus hierdurch gegeben
-        squareMatrix(i, i);
+        SquareMatrix(i, i);
     }
 
-    rekursivFill(0, 0);
+    RekursivFill(0, 0);
 
-    copyField();
+    CopyField();
 
-    clearField(50);
+    ClearField(50);
 
-    copyPlayerField();
+    CopyPlayerField();
 
 }
 
-void printfN(int number) {
+void PrintfN(int number) {
     for (int i = 0; i < number; i++) {
         printf("\n");
     }
@@ -288,16 +288,16 @@ int main(int argc, char **argv) {
     */
     numberLR = 9;
     sqrLR = (int) sqrt(numberLR);
-    genNew();
-    showGameField(kontrollArea);
+    GenNew();
+    ShowGameField(kontrollArea);
 
 
-    printfN(10);
+    PrintfN(10);
     printf("\n              ^");
     printf("\n              |");
     printf("\n           Loesung");
-    printfN(2);
-    play();
+    PrintfN(2);
+    PlayerInput();
     return (EXIT_SUCCESS);
 }
 
